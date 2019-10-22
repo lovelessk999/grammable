59ea294d1f17f8d248b013802455f0c94559a359
Kyle Loveless <lovelessk999@gmail.com>
2019-10-22 16:19:19 +0000

    7 Adding grams to the Datatbase

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -10,7 +10,6 @@ RSpec.describe GramsController, type: :controller do
       expect(response).to have_http_status(:success)
       
     end
-
   end
 
   describe "grams#new action" do
@@ -20,4 +19,13 @@ RSpec.describe GramsController, type: :controller do
     end
   end
 
+  describe "grams#create action" do
+    it "should successfully create a new gram in out database" do
+      post :create, params: { gram: { message: 'Hello!'} }
+      expect(response).to redirect_to root_path
+
+      gram = Gram.last
+      expect(gram.message).to eq("Hello!")
+    end
+  end
 end
